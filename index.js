293f7c801ba8f28bb5a810e2ff2d45b9bffcd66f
Anupam Datta <anupam@Anupams-MacBook-Pro.local>
2026-05-25 23:55:48 +0530

    In memory array implement

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
+let posts = [
+      {
+            id: "1",
+            title: "My First Blog Post",
+            content: "Welcome! This is a mock post stored directly in the server's memory array to verify our EJS looping configurations."
+      }
+];
+
 app.get('/', (req, res) => {
-      res.render('index');
+      res.render('index', { posts: posts });
 });
 
 app.listen(port, () => {
