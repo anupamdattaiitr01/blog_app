9c6e9f88d9f41aeea95e3556d0727c9303b8a369
Anupam Datta <anupam@Anupams-MacBook-Pro.local>
2026-05-26 00:07:39 +0530

    Implement middleware to take care of the blog entry

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 let posts = [
       {
@@ -14,6 +15,28 @@ let posts = [
 
 app.get('/', (req, res) => {
       res.render('index', { posts: posts });
+});
+
+app.get('/posts/new', (req, res) => {
+      res.render('create');
+});
+
+app.post('/posts', (req, res) => {
+      // Destructure form field values directly out of req.body
+      const { title, content } = req.body;
+
+      // Create a new structured post object
+      const newPost = {
+            id: Date.now().toString(), // Generates a clean, unique string ID using a timestamp
+            title: title,
+            content: content
+      };
+
+      // Push the new post object directly into our global in-memory state array
+      posts.push(newPost);
+
+      // Redirect the user back to the home page route to see their new post in the feed!
+      res.redirect('/');
 });
 
 app.listen(port, () => {
